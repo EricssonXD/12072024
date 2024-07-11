7dce60b2c29fa973a39df5ab85d858f2676bf9a5
Ericsson <choericsson@gmail.com>
2024-07-12 02:30:48 +0800

    disable responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,22 @@ import TestImgJpg from './assets/TestImg.jpg';
 import { PhotoStack } from './components/photo-stack/photo-stack';
 import { enableScreenScaler } from "screen-scaler/react";
 
-const { ScreenScalerOutOfRangeFallbackProvider } = enableScreenScaler({
-    rootDivId: "root",
-    targetWindowInnerWidth: ({ zoomFactor, isPortraitOrientation }) =>
-        isPortraitOrientation ? undefined : 1920 * zoomFactor
-});
+// const { ScreenScalerOutOfRangeFallbackProvider } = enableScreenScaler({
+//     rootDivId: "root",
+//     targetWindowInnerWidth: ({ zoomFactor, isPortraitOrientation }) =>
+//         isPortraitOrientation ? undefined : 1920 * zoomFactor,
+// });
 
 function App() {
     return (
-        <ScreenScalerOutOfRangeFallbackProvider
-            fallback={<h1 className={App_module.fallbackText}>Please Rotate your phone, this app does not render well in portrait mode.</h1>}
-        >
-            <div className={App_module.App} >
-                <PhotoStack />
-                <Present className={App_module.presentBox} />
-            </div>
-        </ScreenScalerOutOfRangeFallbackProvider>
+        // <ScreenScalerOutOfRangeFallbackProvider
+        //     fallback={<h1 className={App_module.fallbackText}>Please Rotate your phone, this app does not render well in portrait mode.</h1>}
+        // >
+        <div className={App_module.App} >
+            <PhotoStack />
+            <Present className={App_module.presentBox} />
+        </div>
+        // </ScreenScalerOutOfRangeFallbackProvider>
 
     );
 }
